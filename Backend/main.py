899a1b195f4fb69869cd7a299476e8cd8b56b9d4
Bhavani-V245 <vbhavani245@gmail.com>
2026-05-15 15:54:55 +0530

    change

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from Database.database import engine, Base
+from Routers import auth
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(auth.router)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "FastAPI Auth API"}
